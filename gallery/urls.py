3637fa7d640a4f964cbd849d9c59ef2300d6051e
Lorna-Mwaura <lornamwaura99@gmail.com>
2022-05-30 14:54:13 +0300

    create the url for the search results

--- gallery/urls.py
+++ gallery/urls.py
@@ -2,5 +2,6 @@ from django.urls import re_path
 from . import views
 
 urlpatterns = [
-    re_path('^$', views.welcome)
+    re_path('^$', views.welcome),
+    re_path('search', views.search_results, name='search_results')
 ]
